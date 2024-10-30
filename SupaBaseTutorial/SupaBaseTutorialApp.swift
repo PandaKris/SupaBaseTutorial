93e908ae360794c72e2f7906846389a28c9453e4
Kristanto Sean <kristantosean@gmail.com>
2024-10-30 09:42:23 +0700

    Initial Commit

---
+++ SupaBaseTutorial/SupaBaseTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaBaseTutorialApp.swift
+//  SupaBaseTutorial
+//
+//  Created by Kristanto Sean on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SupaBaseTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
